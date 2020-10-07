13b6e4352c82ac4f24ac6e43fb902301845478f7
Haaris Nadeem <haaris2001@gmail.com>
2020-10-07 11:14:17 -0700

    Problem 1 done

--- src/main/java/Problem1.java
+++ src/main/java/Problem1.java
@@ -1,8 +1,30 @@
 public class Problem1 {
 
     // Do not change signature (function name, parameters)
+    // received help from cite "https://www.geeksforgeeks.org/binary-search/" (used iterative implementation)
     public static int binarySearch(int[] data, int target) {
-        // Add your solution
-        return -1; // Place holder
+
+        int x = 0; // this var narrows down the left side of the array
+        int y = data.length - 1; // this var narrows down the right side of the array
+
+            while(x <= y){
+                int c = x + (y - 1) / 2; // middle value that is used as the index for array to search
+
+                //checks if number chosen is at the center
+                if (data[c] == target){
+
+                    return c;
+
+                } else if(data[c] < target){ // if num is bigger, num is not on the left of the current spot checked
+
+                    x = c + 1;
+
+                } else { // if num is smaller, num is not on the right of the current spot checked
+
+                    y = c - 1;
+
+                }
+            }
+        return -1; // returns -1 if searched num is not in array
     }
 }
